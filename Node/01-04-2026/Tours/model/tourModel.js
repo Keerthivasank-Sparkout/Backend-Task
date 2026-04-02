24d746913919158aa3af98edc7eab716e398c490
Keerthivasak-Sparkout <keerthivasank@sparkouttech.com>
2026-04-02 10:17:24 +0530

    implement the login authendication using otp verifification

---
+++ Node/01-04-2026/Tours/model/tourModel.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose')
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    duration:{
+        type:Number,
+        required:true
+    },
+    maxGroupSize:{
+        type:Number,
+        required:true
+    },
+    difficulty:{
+        type:String,
+        required:true
+    },
+    ratingsAverage: {
+        type: Number,
+        required: true
+    },
+    ratingsQuntity:{
+        type:Number,
+        default:0
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    priceDiscount:{
+        type:Number
+    },
+    summary:{
+        type:String,
+        trim:true
+    },
+    description:{
+        type:String,
+        trim:true
+    },
+    imageCover:{
+        type:String,
+        required:true
+    },
+    images:[String],
+    createdAt:{
+        type:Date,
+        default:Date.now(),
+        select:false
+    },
+    startDates:[Date],
+    is_deleted:{
+        type:Boolean,
+        default:false,
+        select:false
+    }
+},{
+    toJSON:{virtuals:true},
+    toObject:{virtuals:true}
+});
+tourSchema.virtual('durationWeek').get(function(){
+    return this.duration / 7;
+})
+const Tour = mongoose.model('tours', tourSchema);
+module.exports = Tour;
