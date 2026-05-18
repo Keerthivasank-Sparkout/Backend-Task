753e656fa9054a8f983de449421304cb8b8b332c
Keerthivasak-Sparkout <keerthivasank@sparkouttech.com>
2026-05-18 12:51:14 +0530

    add origin to avoid the cors error

--- devops/server/server.js
+++ devops/server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin:"http://localhost:5173"
+  origin:["http://localhost:5173","http://16.170.246.235:5173"]
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
